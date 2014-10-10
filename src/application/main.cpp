88db2241b3fe0cb2503f520455facb759bb55c58
Danijar Hafner <mail@danijar.org>
2014-10-10 13:57:59 +0200

    Print uncaught exceptions

--- src/application/main.cpp
+++ src/application/main.cpp
@@ -1,19 +1,27 @@
+#include <exception>
+#include <iostream>
 #include "../system/system.hpp"
 #include "../module/window/module.hpp"
 
+using namespace std;
 using namespace engine;
 
 int main()
 {
-	system::system system;
+	try {
+		system::system system;
 
-	module::window window("window");
-	system.attach(window);
+		module::window window("window");
+		system.attach(window);
 
-	while (system.running())
-		system.update();
+		while (system.running())
+			system.update();
 
-	system.detach("window");
+		system.detach("window");
+	} catch (std::exception &e) {
+		cerr << "Uncaught exception: " << e.what() << endl;
+		cin.get();
+	}
 
 	return 0;
 }
